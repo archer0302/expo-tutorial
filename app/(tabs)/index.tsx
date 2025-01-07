14f300db0b0d7cb2a025aa99ea20824e981dccb9
Archer Chang <archer.chang32@gmail.com>
2025-01-07 16:08:46 +1100

    add buttons to index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -47,14 +47,20 @@ export default function Index() {
         <ImageViewer imgSource={PlaceholderImage} selectedImageUri={selectedImage} />
       </View>
       {showAppOptions ? (
-        <View />
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+          </View>
+        </View>
       ) : (
         <View style={styles.footerContainer}>
           <Button theme="primary" label="Choose a Photo" onPress={pickImageAsync} />
           <Button label="Use this photo" onPress={() => setShowAppOptions} />
         </View>
       )}
-    </View>
+    </View >
   );
 }
 
@@ -70,5 +76,13 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: 'center',
-  }
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
 });
