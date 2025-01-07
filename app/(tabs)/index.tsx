40f2f94a1c8b95178eb2d3a4afc7fc9402171f8b
Archer Chang <archer.chang32@gmail.com>
2025-01-07 13:35:32 +1100

    add button for stickers

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,8 @@
 import { View, StyleSheet } from 'react-native';
 
 import ImageViewer from '@/components/ImageViewer';
+import CircleButton from '@/components/CircleButton';
+import IconButton from '@/components/IconButton';
 import Button from '@/components/Button';
 import * as ImagePicker from 'expo-image-picker';
 import { useState } from 'react';
@@ -9,20 +11,33 @@ const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ['images'],
-        allowsEditing: true,
-        quality: 1,
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
     })
 
     if (!result.canceled) {
-      console.log(result);
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
       alert('No image selected');
     }
+  }
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  }
+
+  const onAddSticker = () => {
+    // We will implement this.
+  }
+
+  const onSaveImageAsync = () => {
+    // We will implement this.
   }
 
 
@@ -31,10 +46,14 @@ export default function Index() {
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} selectedImageUri={selectedImage} />
       </View>
-      <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a Photo" onPress={pickImageAsync}/>
-        <Button label="Use this photo" />
-      </View>
+      {showAppOptions ? (
+        <View />
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose a Photo" onPress={pickImageAsync} />
+          <Button label="Use this photo" onPress={() => setShowAppOptions} />
+        </View>
+      )}
     </View>
   );
 }
